6aadb3a33411a0b2e89ac1238af258b69cbf98f5
dongxiaohong <dongxiaohong@daokoudai.com>
2018-11-19 11:27:14 +0800

    clear archetect

---
+++ src/test/java/org/dano/boot/GenericXmlApplicationContextTest.java
@@ -0,0 +1,18 @@
+package org.dano.boot;
+
+import org.dano.boot.service.NbService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/**
+ * @author dongxiaohong
+ * @date 2018/11/16 17:05
+ */
+public class GenericXmlApplicationContextTest {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new GenericXmlApplicationContext("application-test.xml");
+        NbService nbService = (NbService) context.getBean("nbService");
+        nbService.vo();
+    }
+}
