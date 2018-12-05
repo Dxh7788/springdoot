68267a21f6b91c898e5bfe88599f2be5253d3a9a
dongxiaohong <dongxiaohong@daokoudai.com>
2018-12-05 15:04:29 +0800

    set add test

--- src/test/java/org/dano/boot/util/ConcurrentUtils.java
+++ src/test/java/org/dano/boot/util/ConcurrentUtils.java
@@ -2,6 +2,9 @@ package org.dano.boot.util;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author dongxiaohong
  * @date 2018/12/4 14:00
@@ -11,5 +14,13 @@ public class ConcurrentUtils {
     @Test
     public void concurrentUtilsTest(){
 
+    }
+    @Test
+    public void setAddTest(){
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("dxh");
+        if (!stringSet.add("dxh")){
+            System.out.println("值已经存在,无法继续添加!!!");
+        }
     }
 }
